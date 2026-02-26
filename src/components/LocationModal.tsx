e9812793bb82a0f4298d2d9a03b40c5dde72abe2
rm004607 <168883230+rm004607@users.noreply.github.com>
2026-02-25 21:47:06 -0300

    arreglo archivos

--- src/components/LocationModal.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * @deprecated This component is no longer used and has been removed from the application.
- * All location functionalities now default to Chile.
- */
-export default function LocationModal() {
-    return null;
-}
